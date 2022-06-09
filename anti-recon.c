ba40c5b42a2aee2d2b255456c6bec611c7b35395
Adrian Brodzik <adrianbrodzyk@gmail.com>
2022-06-09 19:05:18 +0000

    Working checksum calculation.

--- anti-recon.c
+++ anti-recon.c
@@ -1,6 +1,7 @@
 /*
  * References:
  *  https://stackoverflow.com/questions/54073052/capture-all-packages-in-kernel-module-with-netfilter-hooks-after-v4-13-x
+ *  https://stackoverflow.com/a/49698390
  *  https://elixir.bootlin.com/linux/latest/source/include/linux/netfilter.h
  *  https://elixir.bootlin.com/linux/latest/source/include/uapi/linux/tcp.h
  *  https://elixir.bootlin.com/linux/latest/source/include/uapi/linux/ip.h
@@ -32,7 +33,20 @@ unsigned int hook_funcion(void *priv, struct sk_buff *skb, const struct nf_hook_
     struct iphdr *ip = ip_hdr(skb);
     struct tcphdr *tcp = tcp_hdr(skb);
 
-    if (ntohs(tcp->source) == 22 && skb->len > 0)
+    if (ip->protocol == IPPROTO_TCP && skb->len == 44 && tcp->ack == 1 && tcp->syn == 1)
+    {
+        pr_info("len: %d", skb->len);
+        pr_info("ihl: %d", ip->ihl);
+        pr_info("tos: %d", ip->tos);
+        pr_info("tot_len: %d", ip->tot_len);
+        pr_info("id: %d", ip->id);
+        pr_info("frag_off: %d", ip->frag_off);
+        pr_info("ttl: %d", ip->ttl);
+        pr_info("protocol: %d", ip->protocol);
+        pr_info("check: %d", ip->check);
+    }
+
+    /*if (ntohs(tcp->source) == 22 && skb->len > 0)
     {
         char *p = (char *)((char *)tcp + (unsigned int)(tcp->doff * 4));
 
@@ -41,18 +55,13 @@ unsigned int hook_funcion(void *priv, struct sk_buff *skb, const struct nf_hook_
 
         while ((x = strstr(p, template)) != NULL)
         {
-            strncpy(x, "SSH-1.0-OpenSSH_6.3p1 Debian-3debian0.3", strlen(template));
+            strncpy(x, "SSH-2.0-OpenSSH_6.3p1 Debian-1debian0.1", strlen(template));
         }
-    }
+    }*/
 
     if (ntohs(tcp->source) == 80 && skb->len > 0)
     {
         pr_info("src: %pI4 %d dest: %pI4 %d", &ip->saddr, ntohs(tcp->source), &ip->daddr, ntohs(tcp->dest));
-        pr_info("len: %d", skb->len);
-        pr_info("skb checksum: %d", skb->csum);
-        pr_info("tcp checksum: %d", tcp->check);
-        pr_info("ip checksum: %d", ip->check);
-        pr_info("data:");
 
         char *p = (char *)((char *)tcp + (unsigned int)(tcp->doff * 4));
 
@@ -65,12 +74,25 @@ unsigned int hook_funcion(void *priv, struct sk_buff *skb, const struct nf_hook_
         }
     }
 
-    /*if (ip)
+    if (skb)
     {
-        ip->ttl = 128;
+        skb->ip_summed = CHECKSUM_NONE;
+        skb->csum_valid = 0;
+        skb->csum = 0;
+    }
+
+    if (ip)
+    {
         ip->check = 0;
         ip->check = ip_fast_csum((unsigned char *)ip, ip->ihl);
-    }*/
+    }
+
+    if (tcp)
+    {
+        int tcplen = ntohs(ip->tot_len) - ip->ihl * 4;
+        tcp->check = 0;
+        tcp->check = csum_tcpudp_magic(ip->saddr, ip->daddr, tcplen, IPPROTO_TCP, csum_partial((char *)tcp, tcplen, 0));
+    }
 
     return NF_ACCEPT;
 }
